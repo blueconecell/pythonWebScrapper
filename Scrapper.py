a99ffe7e3ef447072ceb3c6d14a0a8dd88918a23
blueconecell <95961799+blueconecell@users.noreply.github.com>
2023-02-21 22:03:11 +0900

    day9

--- Scrapper.py
+++ Scrapper.py
@@ -1,15 +1,51 @@
 from requests import get
-base_url = "https://remoteok.com/remote-"
-search_term = "python"
-end_url = "-jobs"
+from bs4 import BeautifulSoup
 
-# url = f"{base_url}{search_term}{end_url}"
-url = "https://remoteok.com"
-response = get(url)
+def extract_jobs(term):
 
-if response.status_code != 200:
-  print("Can't request website")
-else:
-  print(response.text)
-print(response)
-print(url)
+  url = f"https://remoteok.com/remote-{term}-jobs"
+  response = get(url,headers={"User-Agent": "Chrome"})
+  
+  if response.status_code != 200:
+    print("Can't request website")
+  else:
+    
+    soup = BeautifulSoup(response.text, "html.parser")
+    sections = soup.find_all('td',class_="company_and_position")
+    tags = soup.find_all('td', class_="tags")
+  
+    jobs=[]
+    idx = 0
+    for i in sections:
+      idx+=1
+      job_info = {}
+      job_info['idx'] = idx
+      job_info['title'] = list(i.find_all("h2"))[0].string.strip()
+      job_info['company'] = list(i.find_all("h3"))[0].string.strip()
+      locations = i.find_all("div", class_="location")
+      more_info=[]
+      for j in locations:
+         more_info.append(j.string)
+      job_info['more_info'] = more_info
+      jobs.append(job_info)
+  
+    idx = 0
+    for i in tags:
+      idx+=1
+      job_info = {}
+      tag_dict=[]
+      
+      tag = i.find_all("h3")
+      for j in tag:
+        tag_dict.append(j.string.strip())
+      job_info['tags'] = tag_dict  
+      jobs[idx-1].update(job_info)
+    
+    for k in jobs:
+      
+      print('\n\n')
+      for key_, values_ in k.items():
+        if values_ == []:
+          values_ = "X"
+        print(key_,"==>", values_)
+extract_jobs("react")
---
+++ test.py
@@ -0,0 +1,5 @@
+a = {'tag':['1','2'],'com':'po'}
+b = {'title':'ooo','com':'lwwo'}
+
+for i in a:
+    print(i,a.values())
